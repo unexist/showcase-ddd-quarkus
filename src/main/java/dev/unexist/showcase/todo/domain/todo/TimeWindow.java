08013b7da8e9a6d030eace49c35ac05b6b1436c6
Christoph Kappel <christoph@unexist.dev>
2021-01-06 07:40:02 +0100

    Updated dates

--- src/main/java/dev/unexist/showcase/todo/domain/todo/TimeWindow.java
+++ src/main/java/dev/unexist/showcase/todo/domain/todo/TimeWindow.java
@@ -2,7 +2,7 @@
  * @package Quarkus-DDD-Showcase
  *
  * @file TimeWindow class
- * @copyright 2020 Christoph Kappel <christoph@unexist.dev>
+ * @copyright 2020-2021 Christoph Kappel <christoph@unexist.dev>
  * @version $Id$
  *
  * This program can be distributed under the terms of the GNU GPLv2.
