7e81692837a1d30b7050d7cf2f808fd7fe527f06
Christoph Kappel <christoph@unexist.dev>
2021-04-07 13:37:45 +0200

    Updated GPL version

--- src/main/java/dev/unexist/showcase/todo/domain/todo/TodoService.java
+++ src/main/java/dev/unexist/showcase/todo/domain/todo/TodoService.java
@@ -5,7 +5,7 @@
  * @copyright 2020-2021 Christoph Kappel <christoph@unexist.dev>
  * @version $Id$
  *
- * This program can be distributed under the terms of the GNU GPLv2.
+ * This program can be distributed under the terms of the GNU GPLv3.
  * See the file LICENSE for details.
  **/
 
--- src/test/java/dev/unexist/showcase/todo/application/TodoResourceTest.java
+++ src/test/java/dev/unexist/showcase/todo/application/TodoResourceTest.java
@@ -5,7 +5,7 @@
  * @copyright 2019 Christoph Kappel <unexist@subforge.org>
  * @version $Id$
  *
- * This program can be distributed under the terms of the GNU GPLv2.
+ * This program can be distributed under the terms of the GNU GPLv3.
  * See the file LICENSE for details.
  **/
 
