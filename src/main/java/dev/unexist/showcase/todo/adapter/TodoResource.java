b847bdb455864a363667510c67822a1b8d478093
Christoph Kappel <christoph@unexist.dev>
2021-06-23 14:39:24 +0200

    Updated REST interface

--- src/main/java/dev/unexist/showcase/todo/adapter/TodoResource.java
+++ src/main/java/dev/unexist/showcase/todo/adapter/TodoResource.java
@@ -36,8 +36,11 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -59,11 +62,13 @@ public class TodoResource {
             @APIResponse(responseCode = "406", description = "Bad data"),
             @APIResponse(responseCode = "500", description = "Server error")
     })
-    public Response create(@Valid final TodoDTO todoDto) {
+    public Response create(@Valid final TodoDTO todoDto, @Context UriInfo uriInfo) {
         Response.ResponseBuilder response;
 
         if (this.todoService.create(todoDto)) {
-            response = Response.status(Response.Status.CREATED);
+            URI uri = uriInfo.getAbsolutePathBuilder().build();
+
+            response = Response.created(uri);
         } else {
             response = Response.status(Response.Status.NOT_ACCEPTABLE);
         }
